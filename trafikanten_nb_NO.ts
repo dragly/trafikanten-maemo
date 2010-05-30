ee5c6a4e4a5e8a42a673a622e41c561a68e1d334
Svenn-Arne Dragly <s@dragly.com>
2010-05-30 02:39:57 +0200

    Added translation files

---
+++ trafikanten_nb_NO.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nb_NO">
+<context>
+    <name>DeparturesWindow</name>
+    <message>
+        <location filename="departureswindow.ui" line="14"/>
+        <source>Departures</source>
+        <translation>Avganger</translation>
+    </message>
+    <message>
+        <location filename="departureswindow.ui" line="21"/>
+        <source>Departures from</source>
+        <translation>Avganger fra</translation>
+    </message>
+    <message>
+        <location filename="departureswindow.ui" line="36"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWindow</name>
+    <message>
+        <location filename="searchwindow.ui" line="14"/>
+        <location filename="searchwindow.ui" line="47"/>
+        <source>Search</source>
+        <translation>Søk</translation>
+    </message>
+</context>
+<context>
+    <name>TrafikantenWindow</name>
+    <message>
+        <location filename="trafikantenwindow.ui" line="14"/>
+        <source>Trafikanten</source>
+        <translation>Trafikanten</translation>
+    </message>
+    <message>
+        <location filename="trafikantenwindow.ui" line="34"/>
+        <source>Trafikanten for Maemo gives you real time information about the public transport system in Oslo and other areas of Norway.</source>
+        <translation>Trafikanten for Maemo gir deg sanntidsinfo for kollektivtransporten i Oslo og omegn.</translation>
+    </message>
+    <message>
+        <location filename="trafikantenwindow.ui" line="62"/>
+        <source>Recent</source>
+        <translation>Nylige søk</translation>
+    </message>
+    <message>
+        <location filename="trafikantenwindow.ui" line="69"/>
+        <source>Search</source>
+        <translation>Søk</translation>
+    </message>
+    <message>
+        <location filename="trafikantenwindow.ui" line="76"/>
+        <source>Nearby</source>
+        <translation>I nærheten</translation>
+    </message>
+</context>
+</TS>
